1675213a7f8dce203757962e79a6acadb7251dbe
Nacho López Sáez <32782455+nachols1986@users.noreply.github.com>
2024-01-21 11:44:03 -0300

    Update 04_stored_procedures.sql

--- 04_stored_procedures.sql
+++ 04_stored_procedures.sql
@@ -1,23 +1,26 @@
---Stored Procedures
-
--- 1. Procedimiento para calcular el total gastado por un usuario
--- Este procedimiento acepta como entrada el ID de un usuario y calcula el total gastado por ese usuario en transacciones de compra.
+-- 1. Limpiar las reviews de un usuario
+-- Este procedimiento acepta como entrada el ID de un usuario y elimina todas sus reseñas de la tabla REVIEW
 DELIMITER //
 CREATE PROCEDURE
-  calculatetotalspentbyuser(IN userid      INT,
-                            OUT totalspent DECIMAL(10, 2))
+    eliminarresenasporusuario(IN username_param VARCHAR(50))
 begin
-  SELECT sum(amount)
-  INTO   totalspent
-  FROM   transaction
-  WHERE  buyer_id = userid;
-END;//
+    DECLARE user_id_param INT;
+    -- Obtener el ID del usuario a partir del usernameSELECT id
+    INTO   user_id_param
+    FROM   user
+    WHERE  username = username_param;
+  
+    -- Eliminar las reseñas asociadas al usuarioDELETE
+    FROM   review
+    WHERE  user_id = user_id_param;
+  
+    -- Imprimir mensaje informativo
+  SELECT CONCAT('Se eliminaron las reseñas del usuario ', username_param) AS mensaje;
+  END //
 delimiter ;
 
-
 -- 2. Procedimiento para agregar productos al carrito de compras
--- Este procedimiento permite a un usuario agregar productos a su carrito de compras. Toma como entrada el ID del usuario, el ID del producto y la cantidad a agregar. Luego, inserta una fila en la tabla shoppingcart para registrar el producto en el carrito del usuario.
-DELIMITER //
+-- Este procedimiento permite a un usuario agregar productos a su carrito de compras. Toma como entrada el ID del usuario, el ID del producto y la cantidad a agregar. Luego, inserta una fila en la tabla shoppingcart para registrar el producto en el carrito del usuario.DELIMITER //
 CREATE PROCEDURE
   addproducttoshoppingcart(IN userid    INT,
                            IN productid INT,
@@ -37,19 +40,18 @@ begin
                           quantity,
                           now()
               );
-END;//
-
-delimiter ; 
+  END;//
+delimiter ;
 
 -- 3. Procedimiento para marcar una notificación como leída
--- Este procedimiento permite a un usuario marcar una notificación como leída. Toma como entrada el ID de la notificación y el ID del usuario. Luego, actualiza el estado de la notificación a "leída" en la tabla notification.
-DELIMITER //
+-- Este procedimiento permite a un usuario marcar una notificación como leída. Toma como entrada el ID de la notificación y el ID del usuario. Luego, actualiza el estado de la notificación a "leída" en la tabla notification.DELIMITER //
 CREATE PROCEDURE
   marknotificationasread(IN notificationid INT,
                          IN userid         INT)
 begin
   UPDATE notification
-  SET    READ = TRUE
+  SET    read_ = TRUE
   WHERE  id = notificationid
-  AND    user_id = userid;END;//
+  AND    user_id = userid;
+END;//
 delimiter ;
